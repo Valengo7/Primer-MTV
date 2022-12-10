f0d90a71ae81bea4f204b09fe2fa97936cfbfdd5
Valentin Albornoz <valengo2017@gmail.com>
2022-12-10 15:22:29 -0300

    Entrega

---
+++ seed_data.py
@@ -0,0 +1,6 @@
+from ejemplo.models import Familiar
+Familiar(nombre="Rosario", direccion="Rio Parana 745", numero_pasaporte=123123, fecha_nac="2000-7-7").save()
+Familiar(nombre="Alberto", direccion="Rio Parana 745", numero_pasaporte=890890, fecha_nac="2001-4-16").save()
+Familiar(nombre="Samuel", direccion="Rio Parana 745", numero_pasaporte=345345, fecha_nac="1983-3-13").save()
+Familiar(nombre="Florencia", direccion="Rio Parana 745", numero_pasaporte=567567, fecha_nac="1983-4-12").save()
+print("Se cargo con éxito los usuarios de pruebas")
